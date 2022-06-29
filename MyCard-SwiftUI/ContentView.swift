56c4a30444a124eacf60acd000e143f138e12c90
Thanh Than Van <thanh.than.van@ofatechjsc.com>
2022-06-29 15:11:19 +0700

    Initial Commit

---
+++ MyCard-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCard-SwiftUI
+//
+//  Created by Thanh - iOS on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCard-SwiftUI/MyCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCard_SwiftUIApp.swift
+//  MyCard-SwiftUI
+//
+//  Created by Thanh - iOS on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
